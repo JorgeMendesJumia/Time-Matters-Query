76de804e60487358d70007d9eb957dd970e5b8b4
Jorge Mendes <mendesjorge49@gmail.com>
2019-06-12 20:22:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask_cors import CORS
 from query import time_matters_query
 import os
+import imp
 
 flask_app = Flask(__name__)
 app = Api(app=flask_app)
@@ -14,9 +15,13 @@ CORS(flask_app)
 @name_space.route("/")
 class MyResource(Resource):
     @app.doc(responses={200: 'OK', 400: 'Invalid Argument', 500: 'Mapping Key Error'},
-             params={'query': 'Insert query'})
+             params={'query': 'Insert query', 'offset': 'Start the '})
     def get(self):
         query_text = str(request.args.get('query'))
+        path = imp.find_module('py_heideltime')[1]
+        full_path = path + "/Heideltime/TreeTaggerLinux"
+        command = 'chmod u+x '+ full_path
+        os.popen(command).read()
         json_dates = time_matters_query(query_text, 6)
         return json_dates
 
