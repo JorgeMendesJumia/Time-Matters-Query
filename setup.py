a6ef6b77b4e5c4f426716a8b9457d672c212ce0b
JMendes1995 <mendesjorge49@gmail.com>
2020-02-02 18:21:03 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 setup(name='Time-Matters-Query',
       version='1.0',
-      description=,
+      description="",
       author='Jorge Alexandre Rocha Mendes',
       author_email='mendesjorge49@gmail.com',
       url='https://github.com/JMendes1995/Time-Matters-Query.git',
