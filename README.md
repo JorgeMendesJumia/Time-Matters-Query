47853738cf25788852749ad380de6faada59700c
Jorge Mendes <mendesjorge49@gmail.com>
2020-02-20 13:16:29 +0000

    fix bug and refactor

--- README.md
+++ README.md
@@ -10,23 +10,29 @@ from Time_Matters_Query import Query
 
 max_items=3
 offset=0
-query = Query(max_items, offset)
-domains = ['http://publico.pt/', 'http://www.dn.pt/']
+query = Query(max_items, offset, newspaper3k=True)
+domain = 'http://www.ipt.pt'
+from = '2010'
+to = '2009'
 q = 'guerra na síria'
-result_articles = query.arquivo_pt(q,domains)
+result_articles = query.arquivo_pt(q, url, from, to)
 ```
+#### _Query parameters
+- `max_items` : <b>(optional)</b> Maximum number of items on the response.(Default: 50, Max: 2000) 
+- `offset`: <b>(optional)</b> The position of the text indices where the search begins. (Default: 0)
+- `newspaper3k`: <b>(optional)</b> Flag to return the content extracted by Newspaper3K. (Default: True)
 
 #### _arquivo_pt  parameters_
 - `q` : <b>(requireded)</b> query to search.
-- `max_items` : <b>(optional)</b> Maximum number of items on the response.(Default: 50, Max: 2000) 
-- `offset`: <b>(optional)</b> The position of the text indices where the search begins. (Default: 0)
-- `domains`: <b>(optional)</b> List of domains to search into (e.g. ['http://publico.pt/', 'http://www.dn.pt/'])
+- `url` : <b>(optional)</b> Custom domain (e.g. http://www.ipt.pt) or a list of domains (e.g. ['http://www.publico.pt', 'http://www.jn.pt'])
 - `from` : <b>(optional)</b> Set an initial date for the time span of the search. Format: YYYYMMDDHHMMSS, also accepts a shorter date fotmat, e.g. (YYYY). (Default: 1996)
 - `to` : <b>(optional)</b> Set a end date for the time span of the search. format: YYYYMMDDHHMMSS, also accepts a shorter date format, for example (YYYY). (Default: Current Year-1)
-- `url` : <b>(optional)</b> Custom url  (e.g. https://arquivo.pt/textsearch?versionHistory=http://www.ipt.pt&maxItems=1000&from=2010&to=2016)
 
 ###### Output
-The output will be a list of dictionaries composed by:
+The output will be a list of 2 dictionaries composed by:
+- `time`: execution time of time-matters-query
+- `n_docs`: Number of docs extracted.
+- `n_domains`: Number of domains found in search.
 - `fullContentLenght_Newspaper3K`: full content extracted by Newspaper3K
 - `Summary_Newspaper3k`: summarized content by Newspaper3K
 - `fullContentLenght_Arquivo`: full content extracted by arquivo.pt
@@ -36,9 +42,3 @@ The output will be a list of dictionaries composed by:
 - `url`: arquivo.pt URL
 - `domain`:  list of domains userd to perform the search
 
-### Temporal search with Time-Matters
-```bash
-list_articles = [article['fullContentLenght_Newspaper3K'] for article in result_articles]
-
-temporal_search_multiple_doc = query.Time_Matters_MultipleDocs(articles)
-```
--- Time_Matters_Query/query.py
+++ Time_Matters_Query/query.py
@@ -5,66 +5,113 @@ from Time_Matters_SingleDoc import Time_Matters_SingleDoc
 from Time_Matters_MultipleDocs import Time_Matters_MultipleDocs
 from datetime import datetime
 from newspaper import Article
+import time
 
 class Query():
-    def __init__(self, max_items=50, offset=0):
+    def __init__(self, max_items=50, offset=0, newspaper3k=True):
         self.max_items = max_items
         self.offset = offset
+        self.newspaper3k=newspaper3k
 
-    def arquivo_pt(self, query, max_items=50, offset=0, domains=[], from_date='', to_date='', url=''):
+    def arquivo_pt(self, query,  domains=[], from_date='', to_date='', link=''):
         result_list = []
-        if url == '':
+        site_search=[]
+        d=[]
+        import time
+        start_time = time.time()
+        if not isinstance(domains,list):
+            site_search.append(domains)
+        else:
+            site_search=domains
+        if link == '':
             arquivo_pt = 'http://arquivo.pt/textsearch'
-            domains = ','.join(domains)
+            domains = ','.join(site_search)
             payload = {'q': query,
-                       'maxItems': max_items,
-                       'offset': offset,
-                       'siteSearch': domains,
+                       'maxItems': self.max_items,
+                       'offset': self.offset,
+                       'siteSearch': site_search,
                        'from':from_date,
                        'to':to_date,
                        'fields':'title,originalURL,linkToExtractedText,linkToNoFrame,linkToArchive,tstamp,date,siteSearch,snippet'}
             r = requests.get(arquivo_pt, params=payload)
         else:
-            r = requests.get(url)
+            r = requests.get(link)
 
         contentsJSon = r.json()
         for item in contentsJSon["response_items"]:
-            fullContentLenght_Newspaper3K, Summary_Newspaper3k = self.newspaper3k(item['linkToNoFrame'])
-            page = requests.get(item["linkToExtractedText"])
-
-            fullContentLenght_Arquivo = page.content.decode('utf-8')
-            result = {'fullContentLenght_Newspaper3K':fullContentLenght_Newspaper3K,
-                      'Summary_Newspaper3k':Summary_Newspaper3k,
-                      'fullContentLenght_Arquivo': fullContentLenght_Arquivo,
-                      'snippet':item['snippet'], 'crawledData':item['tstamp'],
-                      'title':item["title"],
-                      'url':item["linkToArchive"],
-                      'domain':domains}
-
+            result, d = self.format_output(item, site_search, d)
             result_list.append(result)
-        return result_list
+
+        total_time = time.time() - start_time
+        domains_list = list(set(d))
+
+        statistical_dict = self.search_statistics(total_time, self.max_items, len(domains_list))
+        print(domains_list)
+
+        final_output=[statistical_dict, result_list]
+        return final_output
 
     def google(self, query):
         from googlesearch import search
         list = []
         for url in search(query, tld='com', start = self.offset, stop=self.max_items):
+            fullContentLenght_Newspaper3K, Summary_Newspaper3k = self.newspaper3k(url)
+
             r = requests.get(url)
             from bs4 import BeautifulSoup
             soup = BeautifulSoup(r.text, 'lxml')
-
+            result = {'fullContentLenght_Newspaper3K':fullContentLenght_Newspaper3K,
+                      'Summary_Newspaper3k':Summary_Newspaper3k,
+                      'fullContentLenght': soup.text.encode().decode('utf-8'),
+                      'url':url}
             #soup = BeautifulSoup(r.text, 'html.parser')
-            list.append(soup.text.encode().decode('utf-8'))
-            text = soup.find_all(text=True)
-            #print(text)
+            list.append(result)
+
+
         return list
 
-    def Time_Matters_MultipleDocs(self, list, temporal_tagger=['rule_based'], time_matters=[], score_type='ByCorpus'):
-        results = Time_Matters_MultipleDocs(list, temporal_tagger=temporal_tagger, time_matters=time_matters, score_type=score_type, debug_mode=False)
-        return results
+    #def Time_Matters_MultipleDocs(self, list, temporal_tagger=['rule_based'], time_matters=[], score_type='ByCorpus'):
+    #    results = Time_Matters_MultipleDocs(list, temporal_tagger=temporal_tagger, time_matters=time_matters, score_type=score_type, debug_mode=False)
+    #    return results
 
-    def newspaper3k(self, url):
+    def newspaper3k_get_text(self, url):
         article = Article(url)
         article.download()
         article.parse()
         article.nlp()
         return article.text, article.summary
+
+    def format_output(self,item, domains, d):
+        import re
+        domain = re.findall('(?:https://.+?/|http://.+?/)',item['originalURL'])
+        d.append(domain[0])
+
+        if self.newspaper3k == True:
+            fullContentLenght_Newspaper3K, Summary_Newspaper3k = self.newspaper3k_get_text(item['linkToNoFrame'])
+
+            result = {'fullContentLenght_Newspaper3K': fullContentLenght_Newspaper3K,
+                      'Summary_Newspaper3k': Summary_Newspaper3k,
+                      'snippet': item['snippet'],
+                      'crawledData': item['tstamp'],
+                      'title': item["title"],
+                      'url': item["linkToArchive"],
+                      'domain': domain[0]}
+        else:
+            page = requests.get(item["linkToExtractedText"])
+
+            fullContentLenght_Arquivo = page.content.decode('utf-8')
+            result = {'fullContentLenght_Arquivo': fullContentLenght_Arquivo,
+                      'snippet': item['snippet'],
+                      'crawledData': item['tstamp'],
+                      'title': item["title"],
+                      'url': item["linkToArchive"],
+                      'domain': domain[0]}
+        return result, d
+
+    def search_statistics(self, total_time, max_items, n_domains):
+        statistical_dict = {
+        'time': total_time,
+        'n_docs': max_items,
+        'n_domains': n_domains
+        }
+        return statistical_dict
