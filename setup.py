b7b23a148188d051da92ecc77c7f458abbc987c4
Jorge Mendes <mendesjorge49@gmail.com>
2020-03-20 21:10:53 +0000

    fix setup

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 
 from setuptools import setup, find_packages
+import os
+requirementPath = 'requirements.txt'
+
+if os.path.isfile(requirementPath):
+    with open('requirements.txt') as f:
+        requires = f.readlines()
+
+install_requires = [item.strip() for item in requires]
+
 setup(name='Time_Matters_Query',
       version='1.0',
       description="",
       author='Jorge Alexandre Rocha Mendes',
       author_email='mendesjorge49@gmail.com',
       url='https://github.com/JMendes1995/Time-Matters-Query.git',
-      packages=find_packages()
+      install_requires=install_requires,
       )
 
